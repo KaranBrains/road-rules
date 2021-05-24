2f4adc6787eef787e8e2d04afbd9c3cc71a8a476
Karan Bains <karanbains.bains@gmail.com>
2021-05-24 20:12:58 +0530

    Added Models for instructor , rides and slots

---
+++ backend/models/Slot.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const SlotSchema = new mongoose.Schema({
+  date: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  clientLimit: {
+    type: Number,
+    required: true,
+  },
+  instructor: {
+    type: Number,
+    required: true,
+  },
+  bookings :[{
+    type: String,
+  }]
+});
+
+module.exports = mongoose.model('Slot', SlotSchema);
