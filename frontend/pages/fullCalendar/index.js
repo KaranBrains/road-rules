f92df08bc4b64342280fe8689c68bd123db35c06
Shruti3004 <mail2shruti.ag@gmail.com>
2021-05-28 14:58:27 +0530

    added pafe for full Calendar

---
+++ frontend/pages/fullCalendar/index.js
@@ -0,0 +1,29 @@
+function Calendar() {
+  const myEventsList = [
+    {
+      id: 1,
+      color: "#fd3153",
+      from: "2019-05-02T18:00:00+00:00",
+      to: "2019-05-05T19:00:00+00:00",
+      title: "This is an event",
+    },
+    {
+      id: 2,
+      color: "#1ccb9e",
+      from: "2019-05-01T13:00:00+00:00",
+      to: "2019-05-05T14:00:00+00:00",
+      title: "This is another event",
+    },
+    {
+      id: 3,
+      color: "#3694DF",
+      from: "2019-05-05T13:00:00+00:00",
+      to: "2019-05-05T20:00:00+00:00",
+      title: "This is also another event",
+    },
+  ];
+
+  return <></>;
+}
+
+export default Calendar;
