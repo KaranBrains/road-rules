8e07ee3a50cd9565a105dc36a2b824b2167370da
Karan Bains <karanbains.bains@gmail.com>
2021-06-15 18:39:11 +0530

    modified the home page

--- frontend/components/home/Features.js
+++ frontend/components/home/Features.js
@@ -5,123 +5,64 @@ import RoomIcon from '@material-ui/icons/Room';
 import DriveEtaIcon from '@material-ui/icons/DriveEta';
 import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
 import GavelIcon from '@material-ui/icons/Gavel';
+import router from "next/router";
 
 function Features() {
   return (
-    <div className="my-5">
+    <div className="my-5 auth-bg">
       <div className={`container ${styles.pySection} px-4`}>
         <h2
-          className={`font-bold text-primaryColor mb-5 ${styles.mobileCenter}`}
+          className={`font-bold text-primaryColor mb-5 text-center ${styles.mobileCenter}`}
         >
-          Your key to safe Driving.<br />
+          Booking in three easy steps <br />
           {/* Smart Drivers start <br />
           here */}
         </h2>
 
         <div className="row">
           <div
-            className={`col-lg-4 col-md-4 col-sm-6 col-12 p-2 ${styles.mobileCenter}`}
+            className={`col-lg-12 col-md-12 col-sm-12 col-12 p-2 ${styles.mobileCenter}`}
           >
-            <div className="card bg-white h-100">
+            <div className="card animated-card bg-white h-100" onClick={()=>{
+              router.push("/auth/login");
+            }}>
               <div className="card-body rounded shadow">
-                <div className="image-container text-center my-3">
-                <LocalActivityIcon className="text-primaryColor mb-2" fontSize="large"></LocalActivityIcon>
-                </div>
-                <h4 className="text-primaryColor mt-4 mb-3 font-18 font-bold text-center">
-                  Choice
+                <h4 className="text-primaryColor mt-4 mb-3 font-20 font-bold">
+                  Step 1
                 </h4>
-                <div className="font-demi text-muted text-center mb-3">
-                We are the best in terms of services that we offer you. We ensure safety as our main priority therefore we are well equipped with professional and well trained instructors.
+                <div className="font-demi text-muted mb-3">
+                Go to Login/Signup and create an account.
                 </div>
               </div>
             </div>
           </div>
           <div
-            className={`col-lg-4 col-md-4 col-sm-6 col-12 p-2 ${styles.mobileCenter}`}
+            className={`col-lg-12 col-md-12 col-sm-12 col-12 p-2 ${styles.mobileCenter}`}
           >
-            <div className="card bg-white h-100">
+            <div className="card animated-card  bg-white h-100" onClick={()=>{
+              router.push("/fullCalendar");
+            }}>
               <div className="card-body rounded shadow">
-                <div className="image-container text-center my-3">
-                  <CheckBoxIcon className="text-primaryColor mb-2" fontSize="large"></CheckBoxIcon>
-                </div>
-                <h4 className="text-primaryColor mt-4 mb-3 font-18 font-bold text-center">
-                  99% PASS RATE
+                <h4 className="text-primaryColor mt-4 mb-3 font-22 font-bold">
+                  Step 2
                 </h4>
-                <div className="font-demi text-muted text-center  mb-3">
-                  Success rate of our students is very high.
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div
-            className={`col-lg-4 col-md-4 col-sm-6 col-12 p-2 ${styles.mobileCenter}`}
-          >
-            <div className="card bg-white h-100">
-              <div className="card-body rounded shadow">
-                <div className="image-container text-center my-3">
-                <DriveEtaIcon className="text-primaryColor mb-2" fontSize="large"></DriveEtaIcon>
-                </div>
-                <h4 className="text-primaryColor mt-4 mb-3 font-18 font-bold text-center">
-                  Best Cars
-                </h4>
-                <div className="font-demi text-muted text-center  mb-3">
-                Both the classroom driving classes as well as online sessions are available with a wide range of cars.
+                <div className="font-demi text-muted  mb-3">
+                  Go to the Bookings tab and select a suitable time slot.
                 </div>
               </div>
             </div>
           </div>
 
           <div
-            className={`col-lg-4 col-md-4 col-sm-6 col-12 p-2 ${styles.mobileCenter}`}
+            className={`col-lg-12 col-md-12 col-sm-12 col-12 p-2 ${styles.mobileCenter}`}
           >
-            <div className="card bg-white h-100">
+            <div className="card animated-card  bg-white h-100">
               <div className="card-body rounded shadow">
-                <div className="image-container text-center my-3">
-                <RoomIcon className="text-primaryColor mb-2" fontSize="large"></RoomIcon>
-                </div>
-                <h4 className="text-primaryColor mt-4 mb-3 font-18 font-bold text-center">
-                  Pickup
+                <h4 className="text-primaryColor mt-4 mb-3 font-22 font-bold">
+                  Step 3
                 </h4>
-                <div className="font-demi text-muted text-center  mb-3">
-                  We provide door pickup. Literally thousands of destinations. No
-                  extra charges.
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div
-            className={`col-lg-4 col-md-4 col-sm-6 col-12 p-2 ${styles.mobileCenter}`}
-          >
-            <div className="card bg-white h-100">
-              <div className="card-body rounded shadow">
-                <div className="image-container text-center my-3">
-                <PeopleAltIcon className="text-primaryColor mb-2" fontSize="large"></PeopleAltIcon>
-                </div>
-                <h4 className="text-primaryColor mt-4 mb-3 font-18 font-bold text-center">
-                  We train all ages.
-                </h4>
-                <div className="font-demi text-muted text-center  mb-3">
-                We have driving instructors who are certified. Apart from that, they are also fit both mentally as well as physically and have many years of driving experience.
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div
-            className={`col-lg-4 col-md-4 col-sm-6 col-12 p-2 ${styles.mobileCenter}`}
-          >
-            <div className="card bg-white h-100">
-              <div className="card-body rounded shadow">
-                <div className="image-container text-center my-3">
-                <GavelIcon className="text-primaryColor mb-2" fontSize="large"></GavelIcon>
-                </div>
-                <h4 className="text-primaryColor mt-4 mb-3 font-18 font-bold text-center">
-                  Road Rules
-                </h4>
-                <div className="font-demi text-muted text-center  mb-3">
-                We will train you correctly with all road rules and regulations.
+                <div className="font-demi text-muted  mb-3">
+                Pay for the booked slots.
                 </div>
               </div>
             </div>
--- frontend/components/home/Work.js
+++ frontend/components/home/Work.js
@@ -1,109 +1,138 @@
-import Image from "next/image";
 import styles from "./Home.module.css";
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import LocalActivityIcon from '@material-ui/icons/LocalActivity';
+import RoomIcon from '@material-ui/icons/Room';
+import DriveEtaIcon from '@material-ui/icons/DriveEta';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import GavelIcon from '@material-ui/icons/Gavel';
 
-function Work() {
+function Features() {
   return (
-    <>
-      <div>
-        <div className="container py-section px-4">
-          <h2 className="text-primaryColor font-bold sub-heading text-center">
-            Our Happy Customers
-          </h2>
-          <div className="row">
-            <div className="col-lg-3 col-md-3 col-sm-6 col-12 px-5 center">
-              <Image
-                src="/images/customerOne.svg"
-                alt="Picture of the author"
-                width={500}
-                height={500}
-              />
+    <div className="my-5">
+      <div className="text-center my-5 container">
+        <img src="/images/about.gif" alt="about" className="img-fluid" />
+      </div>
+      <div className={`container ${styles.pySection} px-4`}>
+        <h2
+          className={`font-bold text-primaryColor mb-5 ${styles.mobileCenter}`}
+        >
+          Your key to safe Driving.<br />
+          {/* Smart Drivers start <br />
+          here */}
+        </h2>
+
+        <div className="row">
+          <div
+            className={`col-lg-4 col-md-4 col-sm-6 col-12 p-2 ${styles.mobileCenter}`}
+          >
+            <div className="card bg-white h-100">
+              <div className="card-body rounded shadow">
+                <div className="image-container text-center my-3">
+                <LocalActivityIcon className="text-primaryColor mb-2" fontSize="large"></LocalActivityIcon>
+                </div>
+                <h4 className="text-primaryColor mt-4 mb-3 font-18 font-bold text-center">
+                  Choice
+                </h4>
+                <div className="font-demi text-muted text-center mb-3">
+                We are the best in terms of services that we offer you. We ensure safety as our main priority therefore we are well equipped with professional and well trained instructors.
+                </div>
+              </div>
             </div>
-            <div className="col-lg-3 col-md-3 col-sm-6 col-12 px-5 center">
-              <Image
-                src="/images/customerTwo.png"
-                alt="Happy Customer"
-                width={500}
-                height={500}
-              />
+          </div>
+          <div
+            className={`col-lg-4 col-md-4 col-sm-6 col-12 p-2 ${styles.mobileCenter}`}
+          >
+            <div className="card bg-white h-100">
+              <div className="card-body rounded shadow">
+                <div className="image-container text-center my-3">
+                  <CheckBoxIcon className="text-primaryColor mb-2" fontSize="large"></CheckBoxIcon>
+                </div>
+                <h4 className="text-primaryColor mt-4 mb-3 font-18 font-bold text-center">
+                  99% PASS RATE
+                </h4>
+                <div className="font-demi text-muted text-center  mb-3">
+                  Success rate of our students is very high.
+                </div>
+              </div>
             </div>
-            <div className="col-lg-3 col-md-3 col-sm-6 col-12 px-5 center">
-              <Image
-                src="/images/customerThree.png"
-                alt="Happy Customer"
-                width={500}
-                height={200}
-              />
+          </div>
+
+          <div
+            className={`col-lg-4 col-md-4 col-sm-6 col-12 p-2 ${styles.mobileCenter}`}
+          >
+            <div className="card bg-white h-100">
+              <div className="card-body rounded shadow">
+                <div className="image-container text-center my-3">
+                <DriveEtaIcon className="text-primaryColor mb-2" fontSize="large"></DriveEtaIcon>
+                </div>
+                <h4 className="text-primaryColor mt-4 mb-3 font-18 font-bold text-center">
+                  Best Cars
+                </h4>
+                <div className="font-demi text-muted text-center  mb-3">
+                Both the classroom driving classes as well as online sessions are available with a wide range of cars.
+                </div>
+              </div>
             </div>
-            <div className="col-lg-3 col-md-3 col-sm-6 col-12 px-5 center">
-              <Image
-                src="/images/customerFour.png"
-                alt="Happy Customer"
-                width={500}
-                height={200}
-              />
+          </div>
+
+          <div
+            className={`col-lg-4 col-md-4 col-sm-6 col-12 p-2 ${styles.mobileCenter}`}
+          >
+            <div className="card bg-white h-100">
+              <div className="card-body rounded shadow">
+                <div className="image-container text-center my-3">
+                <RoomIcon className="text-primaryColor mb-2" fontSize="large"></RoomIcon>
+                </div>
+                <h4 className="text-primaryColor mt-4 mb-3 font-18 font-bold text-center">
+                  Pickup
+                </h4>
+                <div className="font-demi text-muted text-center  mb-3">
+                  We provide door pickup. Literally thousands of destinations. No
+                  extra charges.
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className={`col-lg-4 col-md-4 col-sm-6 col-12 p-2 ${styles.mobileCenter}`}
+          >
+            <div className="card bg-white h-100">
+              <div className="card-body rounded shadow">
+                <div className="image-container text-center my-3">
+                <PeopleAltIcon className="text-primaryColor mb-2" fontSize="large"></PeopleAltIcon>
+                </div>
+                <h4 className="text-primaryColor mt-4 mb-3 font-18 font-bold text-center">
+                  We train all ages.
+                </h4>
+                <div className="font-demi text-muted text-center  mb-3">
+                We have driving instructors who are certified. Apart from that, they are also fit both mentally as well as physically and have many years of driving experience.
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className={`col-lg-4 col-md-4 col-sm-6 col-12 p-2 ${styles.mobileCenter}`}
+          >
+            <div className="card bg-white h-100">
+              <div className="card-body rounded shadow">
+                <div className="image-container text-center my-3">
+                <GavelIcon className="text-primaryColor mb-2" fontSize="large"></GavelIcon>
+                </div>
+                <h4 className="text-primaryColor mt-4 mb-3 font-18 font-bold text-center">
+                  Road Rules
+                </h4>
+                <div className="font-demi text-muted text-center  mb-3">
+                We will train you correctly with all road rules and regulations.
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div>
-        <div className="container px-4 my-5">
-          <h2 className="text-primaryColor font-bold sub-heading text-center">
-            How it works
-          </h2>
-          <div className="row how-it-works pt-5">
-            <div className="col-lg-4 mt-2 col-md-4 col-sm-12 col-12">
-              <div className="d-flex justify-content-center">
-                <div
-                  className={`${styles.workNumber} text-primaryColor font-bold center`}
-                  style={{ fontSize: "35px" }}
-                >
-                  1
-                </div>
-              </div>
-              <h4 className="text-primaryColor font-20 mt-5 font-bold text-center">
-              Confirm your slot.
-              </h4>
-              <p className="text-muted text-center font-demi mb-3 mt-2 font-16">
-                Choose from all the available slots. We accept online payment as well as cash on site .
-              </p>
-            </div>
-            <div className="col-lg-4 mt-2 col-md-4 col-sm-12 col-12">
-              <div className="d-flex justify-content-center">
-                <div
-                  className={`${styles.workNumber} text-primaryColor font-bold center`}
-                  style={{ fontSize: "35px" }}
-                >
-                  2
-                </div>
-              </div>
-              <h4 className="text-primaryColor font-20 mt-5 font-bold text-center">
-                Attend your classes.
-              </h4>
-              <p className="text-muted text-center font-demi mb-3 mt-2 font-16">
-                Upon finishing class , you can provide your feedback.
-              </p>
-            </div>
-            <div className="col-lg-4 mt-2 col-md-4 col-sm-12 col-12">
-              <div className="d-flex justify-content-center">
-                <div
-                  className={`${styles.workNumber} text-primaryColor font-bold center`}
-                  style={{ fontSize: "35px" }}
-                >
-                  3
-                </div>
-              </div>
-              <h4 className="text-primaryColor font-20 mt-5 font-bold text-center">
-                Get your Drivers License.
-              </h4>
-              <p className="text-muted text-center font-demi mb-3 mt-2 font-16">
-                99% of our students pass their test on the first attempt.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    </div>
   );
 }
 
-export default Work;
+export default Features;
