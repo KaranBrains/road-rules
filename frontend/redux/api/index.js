a736abffdb60759817868b5cd98f14a7d70f9657
Karan Bains <karanbains.bains@gmail.com>
2021-06-17 21:49:01 +0530

    made the site live

--- frontend/redux/api/index.js
+++ frontend/redux/api/index.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-export const url = "http://localhost:8080/api";
-export const paymentUrl = "http://localhost:8080/api/create-checkout-session";
-export const paymentUrlBooking = "http://localhost:8080/api/create-checkout-session-booking";
-export const baseUrl = "http://localhost:8080/";
+// export const url = "http://localhost:8080/api";
+// export const paymentUrl = "http://localhost:8080/api/create-checkout-session";
+// export const paymentUrlBooking = "http://localhost:8080/api/create-checkout-session-booking";
+// export const baseUrl = "http://localhost:8080/";
 
-// export const url = "https://www.pigameapp.com:1234/api";
-// export const baseUrl = "https://www.pigameapp.com:1234/";
-// export const paymentUrlBooking = "https://www.pigameapp.com:1234/api/create-checkout-session-booking";
-// export const paymentUrl = "https://www.pigameapp.com:1234/api/create-checkout-session";
+export const url = "https://www.pigameapp.com:1234/api";
+export const baseUrl = "https://www.pigameapp.com:1234/";
+export const paymentUrlBooking = "https://www.pigameapp.com:1234/api/create-checkout-session-booking";
+export const paymentUrl = "https://www.pigameapp.com:1234/api/create-checkout-session";
 // rebuild
 
 export const signIn = (formData) => axios.post(`${url}/login`, formData);
